f7dc6fbed0a68cbc3e14b21bdd449fef6adcbcbe
Rayssa Santos <99840987+RayssaSantosL@users.noreply.github.com>
2024-02-08 02:17:44 -0300

    Exercicio 5
    5) Escreva um programa que inverta os caracteres de um string.
    
    IMPORTANTE:
    a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
    b) Evite usar funções prontas, como, por exemplo, reverse;
    
    
    Codigo escrito em PYTHON.
    By Rayssa Santos.

---
+++ Exercicio 5/exer5.py
@@ -0,0 +1,7 @@
+def inverter_string(string):
+    return string[::-1]
+
+# Exemplo de uso
+entrada = input("Digite uma string: ")
+resultado = inverter_string(entrada)
+print("String invertida:", resultado)
